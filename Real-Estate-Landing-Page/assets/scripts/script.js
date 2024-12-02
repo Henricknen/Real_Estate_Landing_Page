d87aa2698f8b6e77a54b6cdf2ddd4898172d243f
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-12-02 17:56:33 -0300

    concluindo_segundo_desafio_'alterando_imagem'_da_classe_hero-content

--- Real-Estate-Landing-Page/assets/scripts/script.js
+++ Real-Estate-Landing-Page/assets/scripts/script.js
@@ -1,4 +1,11 @@
 // 1
 var element = document.getElementById('substiticao');       // seleciona o elemento com o ID "substiticao"
 
-element.textContent = 'Bem-vindo ao Nosso Site';        // altera o texto para "Bem-vindo ao Nosso Site"  
+element.textContent = 'Bem-vindo ao Nosso Site';        // altera o texto para "Bem-vindo ao Nosso Site"
+
+// 2
+window.onload = function() {        // espera pelo carregamento completo da página
+    var imagemHeroContent = document.querySelector('.hero-content img');        // 'seleçiona' a imagem dentro da classe hero-content
+    
+    imagemHeroContent.src = 'assets/images/new_search.png';         // substitui o atributo src pela nova imagem
+  };
