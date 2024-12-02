475f96c0ad236541e5eb794e02aebb0ec6978d90
Luis Henrique Silva Ferreira <l.henrick@live.com>
2024-12-02 17:47:36 -0300

    concluindo_primeiro_desafio_'alterando_texto'

---
+++ Real-Estate-Landing-Page/assets/scripts/script.js
@@ -0,0 +1,4 @@
+// 1
+var element = document.getElementById('substiticao');       // seleciona o elemento com o ID "substiticao"
+
+element.textContent = 'Bem-vindo ao Nosso Site';        // altera o texto para "Bem-vindo ao Nosso Site"  
